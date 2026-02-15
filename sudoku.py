6216afb5da1ad8c70360596ff45c32323e05d240
Dicleern <github-dicleurun500@gmail.com>
2026-02-15 19:01:33 +0300

    sudoku validation completed

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,29 @@
-# pylint: disable=missing-docstring
-
-
 def sudoku_validator(grid):
-    pass  # YOUR CODE HERE
+    for i in range(9):
+        row = set()
+        column = set()
+        box = set()
+
+        for j in range(9):
+
+            if grid[i][j] != 0:
+                if grid[i][j] in row: return False
+                row.add(grid[i][j])
+
+
+            if grid[j][i] != 0:
+                if grid[j][i] in column: return False
+                column.add(grid[j][i])
+
+            row_start = 3 * (i // 3)
+            col_start = 3 * (i % 3)
+
+
+            r = row_start + j // 3
+            c = col_start + j % 3
+
+            if grid[r][c] != 0:
+                if grid[r][c] in box: return False
+                box.add(grid[r][c])
+
+    return True
